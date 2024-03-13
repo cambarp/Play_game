de5d090b9cb7e6462550b50428cc332d612a6302
ncp <nilcambar@gmail.com>
2024-03-13 18:02:18 -0500

    creando logica

--- src/app/marcador-equipos/marcador-equipos.component.ts
+++ src/app/marcador-equipos/marcador-equipos.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { ResultadosPartidoComponent } from '../resultados-partido/resultados-partido.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-marcador-equipos',
   standalone: true,
-  imports: [],
+  imports: [ResultadosPartidoComponent , FormsModule],
   templateUrl: './marcador-equipos.component.html',
   styleUrl: './marcador-equipos.component.css'
 })
 export class MarcadorEquiposComponent {
-
+  obetener=0;
+    recivirdato(dato : any){
+      this.obetener=dato;
+    }
 }
--- src/app/resultados-partido/resultados-partido.component.ts
+++ src/app/resultados-partido/resultados-partido.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-resultados-partido',
@@ -8,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrl: './resultados-partido.component.css'
 })
 export class ResultadosPartidoComponent {
+  @Output()
+  propagar = new EventEmitter<number>();
+  
 
+  enviar(){
+    this.propagar.emit(this.anotar_equipo_uno + this.anotar_equipo_dos);
+  }
+      anotar_equipo_uno=0;
+      anotar_equipo_dos=0;
+       Anotar(){
+         this.anotar_equipo_uno++;
+       }
+
+       Anotar2(){
+         this.anotar_equipo_dos++;
+        
+       }
+       per_ganar(){
+        if (this.anotar_equipo_uno > this.anotar_equipo_dos){
+          console.log("Equipo uno vas ganando", this.anotar_equipo_uno);
+        }
+        else{
+          console.log("Equipo uno vas perdiendo", this.anotar_equipo_uno);
+        }
+        if (this.anotar_equipo_dos > this.anotar_equipo_uno){
+          console.log("Equipo dos vas ganando", this.anotar_equipo_dos);
+        }
+        else{
+          console.log("Equipo dos vas perdiendo", this.anotar_equipo_dos);
+        }
+       }
 }
