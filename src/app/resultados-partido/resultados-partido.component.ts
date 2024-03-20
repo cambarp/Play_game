38bfcf4e0c8c7ad62f439692de2d77f7ef237115
nilcambar@gmail.com <nilcambar@gmail.com>
2024-03-20 11:34:03 -0500

    haciendo ajustes

--- src/app/resultados-partido/resultados-partido.component.ts
+++ src/app/resultados-partido/resultados-partido.component.ts
@@ -1,22 +1,22 @@
 
 import { Component, Output, EventEmitter,Input} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-resultados-partido' ,
   standalone: true,
-  imports:[],
+  imports:[FormsModule],
   templateUrl: './resultados-partido.component.html',
   styleUrls: ['./resultados-partido.component.css'],
 })
 export class ResultadosPartidoComponent {
   @Output() enviarValores = new EventEmitter<{ equipoUno: number, equipoDos: number }>();
-  
-
   @Input() equipo1: string ='';
   @Input() equipo2: string='';
   anotar_equipo_uno=0;
   anotar_equipo_dos=0;
-  tiempo: number = 60;
+  tiempo: number = 1;
+  tiempo2: number=0;
   intervalo: any;
   estado='';
   botonIniciarActivo: boolean = false;
@@ -63,16 +63,24 @@ export class ResultadosPartidoComponent {
     this.visible_dos=false;
     this.iniciarTemporizador();
   }
+  
   iniciarTemporizador() {
+    this.tiempo2 = this.tiempo * 60;
     this.intervalo = setInterval(() => {
-      if (this.tiempo > 0) {
-        this.tiempo--;
+      if (this.tiempo2 > 0) {
+        this.tiempo2--;
       } else {
         clearInterval(this.intervalo);
-        this.visible_uno=true;
-        this.visible_dos=true;
+        this.visible_uno = true;
+        this.visible_dos = true;
       }
-    }, 1000); 
+    }, 1000);
   }
+  mostrarTiempoRestante(): string {
+    const minutos = Math.floor(this.tiempo2 / 60);
+    const segundos = this.tiempo2 % 60;
+    return `${minutos} M / ${segundos} S`;
+  }
+
       
 }
